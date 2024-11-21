db4960a9198e7df17804bc91ae963a49396a207a
Luis Avendano <leavendano@gmail.com>
2024-11-21 03:24:25 -0600

    Correo Redencion

--- src/Salpat.Clientes.UseCases/Redenciones/Create/CreateRedencionHandler.cs
+++ src/Salpat.Clientes.UseCases/Redenciones/Create/CreateRedencionHandler.cs
@@ -3,11 +3,13 @@ using Ardalis.SharedKernel;
 using Microsoft.EntityFrameworkCore;
 using Salpat.Clientes.Core.ClienteAggregate;
 using Salpat.Clientes.Core.Interfaces;
+using Salpat.Clientes.Core.RecompensaAgreggate;
 using Salpat.Clientes.Core.RedencionAggregate;
 
 
 namespace Salpat.Clientes.UseCases.Redenciones.Create;
-public class CreateRedencionHandler(IRepository<Redencion> _repository, IRepository<Cliente> _repoClientes, IEmailSender emailSender) : 
+public class CreateRedencionHandler(IRepository<Redencion> _repository, IRepository<Cliente> _repoClientes,
+  IRepository<Recompensa> _repoRecompensa, IEmailSender emailSender) : 
     ICommandHandler<CreateRedencionCommand, Result<int>>
 {
   public async Task<Result<int>> Handle(CreateRedencionCommand request, CancellationToken cancellationToken)
@@ -23,15 +25,28 @@ public class CreateRedencionHandler(IRepository<Redencion> _repository, IReposit
         {
           return Result<int>.Conflict("El cliente no cuenta con los puntos requeridos");
         }
+
+        var recompensa = await _repoRecompensa.GetByIdAsync(request.RecompensaId);
+        string nombreRecompensa = recompensa?.Descripcion ?? "";
+        string rootpath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "wwwroot");
+        string htmlmessage = File.ReadAllText(rootpath + "/email_redencion.html");
+
         var createdItem = await _repository.AddAsync(newItem, cancellationToken);
         existingCliente.RedimirPuntos(request.PuntosRedimidos);
         await _repoClientes.UpdateAsync(existingCliente, cancellationToken);
+
+        var mensaje = $"Canjeaste {request.PuntosRedimidos} puntos por {nombreRecompensa}";
+        htmlmessage = htmlmessage.Replace("{{nombre_cliente}}",existingCliente.Nombre);
+        htmlmessage = htmlmessage.Replace("{{saldo_puntos}}", existingCliente.SaldoPuntos.ToString());
+        htmlmessage = htmlmessage.Replace("{{mensaje}}", mensaje);
+
+
         await emailSender.SendEmailAsync(existingCliente.Email,
-                                   "cfdi@infinitummail.com",
+                                   "lealtad.salpat@gmail.com",
                                    "Canjeaste puntos en Salpat!",
                                    $"Hola {existingCliente.Nombre}" + Environment.NewLine +
-                                   $"Redimiste  {request.PuntosRedimidos} puntos, tu saldo ahora es {existingCliente.SaldoPuntos} puntos." +
-                                   $"Recuerda que con Salpat siempre ganas!");
+                                   $"Consumiste  {request.PuntosRedimidos} puntos, tu saldo ahora es {existingCliente.SaldoPuntos} puntos." +
+                                   $"Recuerda que con Salpat siempre ganas!", htmlmessage);
         return createdItem.Id;
       }
       else
